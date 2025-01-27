ff0e99e510a52f50cce45f422557ccff7efa403d
Ilie Pandia <ipandia@brandmovers.com>
2025-01-27 06:36:30 -0800

    The code is working but it highly coupled.

--- app/Console/Commands/RunSimulation.php
+++ app/Console/Commands/RunSimulation.php
@@ -26,31 +26,14 @@ class RunSimulation extends Command
      */
     protected $description = 'Command description';
 
-    protected function drawLayout( array $progress, array $logs)
-    {
-        $totalBars = count($progress);
+    protected array $requests = [];
 
-        $cursor = new Cursor($this->output);
+    protected array $logs = [];
 
-        $cursor->moveToPosition(1, 0 );
-        $this->output->write( "<complete>Requests Progress:</complete>\n");
-
-        // Draw progress bars
-        foreach ($progress as $index => $value) {
-            $bar = str_repeat('=', (int)($value / 1)) . str_repeat(' ', 100 - (int)($value / 1));
-            $style = "complete";
-            if($value < 100){
-                $style = "gray";
-            }
-            $this->output->write(sprintf("<$style>   Req %2d: [%s] %d%%</$style>\n", $index + 1, $bar, $value));
-        }
-
-        // Draw logs below progress bars
-        $cursor->moveToPosition(1, $totalBars + 2 );
-        $this->output->write("<complete>Logs:</complete>\n");
-        foreach (array_slice($logs, -10) as $log) { // Show the last 10 logs
-            $this->output->write("  <gray>" . $log . "</gray>\n");
-        }
+    protected function logMessage($message){
+        $message = is_string($message) ? $message : json_encode( $message );
+        $this->logs[] = $message;
+        $this->logs = array_slice($this->logs, -20);
     }
 
     public function handle()
@@ -59,67 +42,121 @@ class RunSimulation extends Command
            'base_uri' => 'http://localhost:9010/'
         ]);
 
-        $promise = $client->getAsync("/", [
-            'query' => [
-                'size' => 10,
-                'chunks' => 10,
-                'delay' => 500,
+        //ToDo - this will be read from a file
+        $configuration = [
+            'type' => 'async',
+            'requests' => [
+                [
+                    'size' => 10,
+                    'chunks' => 10,
+                    'delay' => 500,
+                ],
+                [
+                    'size' => 20,
+                    'chunks' => 10,
+                    'delay' => 1000,
+                ],
+                [
+                    'size' => 20_000,
+                    'chunks' => 14,
+                    'delay' => 300,
+                ],
+                [
+                    'size' => 3,
+                    'chunks' => 20,
+                    'delay' => 300,
+                ],
+                [
+                    'size' => 30,
+                    'chunks' => 25,
+                    'delay' => 300,
+                ],
+                [
+                    'size' => 30,
+                    'chunks' => 200,
+                    'delay' => 50,
+                ],
             ],
-            'progress' => function($downloadTotal,
-                                   $downloadedBytes,
-                                   $uploadTotal,
-                                   $uploadedBytes){
-                $this->line("Progress 1... $downloadedBytes of $downloadTotal ... $uploadedBytes of $uploadTotal");
-            },
-        ]);
-
-        $promise2 = $client->getAsync("/", [
-            'query' => [
-                'size' => 20,
-                'chunks' => 10,
-                'delay' => 1000,
-            ],
-            'progress' => function($downloadTotal,
-                                   $downloadedBytes,
-                                   $uploadTotal,
-                                   $uploadedBytes){
-                $this->line("Progress 2... $downloadedBytes of $downloadTotal ... $uploadedBytes of $uploadTotal");
-            },
-        ]);
-
-        $promise3 = $client->getAsync("/", [
-            'query' => [
-                'size' => 3,
-                'chunks' => 20,
-                'delay' => 300,
-            ],
-            'progress' => function($downloadTotal,
-                                   $downloadedBytes,
-                                   $uploadTotal,
-                                   $uploadedBytes){
-                $this->line("Progress 3... $downloadedBytes of $downloadTotal ... $uploadedBytes of $uploadTotal");
-            },
-        ]);
-
-        $promises = [
-            'A' => $promise,
-            'B' => $promise2,
-            'C' => $promise3,
         ];
-        $responses = Utils::unwrap($promises);
 
-        foreach( $responses as $key => $response){
-            dump($response->getBody()->getContents());
+        $this->requests = [];
+
+        $promises = [];
+        foreach($configuration['requests'] as $key => $configuration ){
+            $request = [
+                'id' => $key + 1,
+                'status' => 'waiting',
+                'progress' => 0,
+            ];
+            $id = $request['id'];
+            $this->requests[$request['id']] = $request;
+            $promise =  $client->getAsync("/", [
+                'query' => [
+                    'size' => $configuration['size'],
+                    'chunks' => $configuration['chunks'],
+                    'delay' => $configuration['delay'],
+                ],
+                'progress' => function($downloadTotal, $downloadedBytes) use ($id){
+                    $this->requests[$id]['status'] = 'receiving';
+                    if($downloadTotal == 0 || $downloadedBytes == 0 ){
+                        $this->requests[$id]['progress'] = 0;
+                    }else{
+                        $this->requests[$id]['progress'] = ceil( floatval($downloadedBytes) / $downloadTotal  * 100 );
+                    }
+                    if($this->requests[$id]['progress'] == 100 ){
+                        $this->requests[$id]['status'] = 'complete';
+                    }
+                    $this->logMessage("Received progress report for request $id: {$this->requests[$id]['progress']}%...");
+                    $this->updateScreen();
+                },
+            ]);
+            $this->logMessage("Initialized request: {$id}...");
+            $promises []= $promise;
+        }
+        $this->initDisplay();
+        $responses = Utils::unwrap($promises);
+        foreach ($this->requests as $request){
+            $this->requests[$request['id']]['status'] ='complete';
+            $this->logMessage("Request {$request['id']} is complete.");
+        }
+        $this->updateScreen();
+    }
+
+    protected function updateScreen() : void
+    {
+        $totalBars = count($this->requests);
+
+        $cursor = new Cursor($this->output);
+
+        $cursor->moveToPosition(1, 0 );
+        $this->output->write( "<complete>Requests Progress:</complete>\n");
+
+        // Draw progress bars
+        foreach ($this->requests as $index => $request) {
+            $value = $request['progress'];
+            $status = strtoupper(substr($request['status'], 0, 1));
+            $bar = str_repeat('=', (int)($value / 1)) . str_repeat(' ', 100 - (int)($value / 1));
+            $style = "complete";
+            if($value < 100){
+                $style = "gray";
+            }
+            $this->output->write(sprintf("<$style>   Req %2d: (%s) [%s] %d%%</$style>\n", $request['id'], $status, $bar, $value));
+        }
+
+        // Draw logs below progress bars
+        $logs = $this->logs;
+        $cursor->moveToPosition(1, $totalBars + 2 );
+        $this->output->write("<complete>Logs:</complete>\n");
+        foreach (array_slice($logs, -10) as $log) { // Show the last 10 logs
+            $this->output->write("  <gray>" . $log . "</gray>");
+            $cursor->clearLineAfter();
+            $cursor->moveDown();
+            $cursor->moveToColumn(0);
         }
     }
-    /**
-     * Execute the console command.
-     */
-    public function demoDisplay()
-    {
-        $loop = Loop::get();
-        $totalBars = 10;
 
+    protected function initDisplay()
+    {
         //Get the size of the terminal - I might use this to shrink the progress bars
         $terminal = new Terminal();
         $width = $terminal->getWidth();
@@ -134,36 +171,6 @@ class RunSimulation extends Command
         $this->getOutput()->getFormatter()->setStyle('gray', $style );
         $style = new OutputFormatterStyle('white', 'default', ['bold']);
         $this->getOutput()->getFormatter()->setStyle('complete', $style );
-
-        $progress = array_fill(0, $totalBars, 0 );
-
-        $logs = [];
-
-        $loop->addPeriodicTimer(0.1, function() use (&$progress, &$logs, $totalBars){
-            for ($i = 0; $i < $totalBars; $i++) {
-                if ($progress[$i] < 100) {
-                    $progress[$i] += rand(1, 5); // Random progress increment
-                    if ($progress[$i] > 100) {
-                        $progress[$i] = 100;
-                    }
-                }
-            }
-
-            // Add a new log entry
-            $logs[] = "Progress updated at " . date('H:i:s');
-
-            // Redraw the layout
-            $this->drawLayout($progress, $logs);
-        });
-
-        // Stop the loop once all progress bars are at 100%
-        $loop->addPeriodicTimer(0.5, function () use (&$progress, $loop) {
-            if (count(array_filter($progress, fn($p) => $p < 100)) === 0) {
-                $loop->stop();
-            }
-        });
-
-        $loop->run();
-
     }
+
 }
